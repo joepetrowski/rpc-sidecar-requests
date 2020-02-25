8f17ee3dd83491cee5c5a891f4670b0ab4976c61
joepetrowski <joe@parity.io>
2020-02-25 14:02:28 +0100

    tidy up

--- sidecar.py
+++ sidecar.py
@@ -1,4 +1,7 @@
-#%% Imports
+#%% INFO
+# Simple script to fetch block info from a Substrate node using:
+# https://github.com/paritytech/substrate-api-sidecar
+#
 import requests
 import json
 
@@ -8,19 +11,28 @@ max_block = 775_001
 base_url = 'http://127.0.0.1:8080'
 path = 'block'
 
+def print_block_info(block_info):
+	print(
+		'Block {:>9,} has state root {}'.format(
+			block_info['number'], block_info['stateRoot']
+		)
+	)
+
+def process_response(response, block_number):
+	if response.ok:
+		block_info = json.loads(response.text)
+		assert(block_info['number'] == block_number)
+		if block_info['number'] % 5000 == 0:
+			# Print some info... really just to show that it's making progress.
+			print_block_info(block_info)
+	else:
+		print('Response Error: {}'.format(response.status_code))
+		block_info = {
+			'number' : block_number,
+			'Response Error' : response.status_code
+		}
+
 for block in range(0, max_block):
 	response = requests.get(base_url + '/' + path + '/' + str(block))
-	if response.ok:
-		response_text = response.text
-		response_json = json.loads(response_text)
-		
-		assert(response_json['number'] == block)
-		responses.append(response_json)
-
-		if response_json['number'] % 1000 == 0:
-			print(
-				'Block {:>9,} has state root {}'
-				.format(response_json['number'], response_json['stateRoot'])
-			)
-	else:
-		print('Error: {}'.format(response.status_code))
+	block_info = process_response(response, block)
+	responses.append(block_info)
