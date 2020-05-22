56922fec02d0447a72400f2755152ec08c8cccd6
joepetrowski <joe@parity.io>
2020-05-22 20:47:52 +0200

    use class

--- getartifacts.py
+++ getartifacts.py
@@ -2,13 +2,15 @@
 # Example
 import requests
 import json
+from sidecar import Sidecar
 
-url = 'http://127.0.0.1:8080/tx/artifacts'
-response = requests.get(url)
-if response.ok:
-	artifacts = json.loads(response.text)
-	print('\nBlock Number: {}'.format(hex(int(artifacts['at']['height']))))
-	print('\nBlock Hash:   {}'.format(artifacts['at']['hash']))
-	print('\nSpec Version: {}'.format(artifacts['specVersion']))
-	print('\nTx Version:   {}'.format(artifacts['txVersion']))
-	print('\nGenesis Hash: {}'.format(artifacts['genesisHash']))
+url = 'http://127.0.0.1:8080'
+sidecar = Sidecar(url)
+
+artifacts = sidecar.artifacts()
+
+print('\nBlock Number: {}'.format(hex(int(artifacts['at']['height']))))
+print('\nBlock Hash:   {}'.format(artifacts['at']['hash']))
+print('\nSpec Version: {}'.format(artifacts['specVersion']))
+print('\nTx Version:   {}'.format(artifacts['txVersion']))
+print('\nGenesis Hash: {}'.format(artifacts['genesisHash']))
--- getblock.py
+++ getblock.py
@@ -2,12 +2,11 @@
 # Example
 import requests
 import json
+from sidecar import Sidecar
 
-# Get a block by number.
-url = 'http://127.0.0.1:8080/block/2077200'
-response = requests.get(url)
-if response.ok:
-	block_by_number = json.loads(response.text)
-	# print(block_by_number)
-else:
-	print(response.text)
+url = 'http://127.0.0.1:8080'
+# url = 'https://cb-runtime-wk8yx7pds0ag.paritytech.net/block/540'
+
+sidecar = Sidecar(url)
+
+block = sidecar.block(1184728)
