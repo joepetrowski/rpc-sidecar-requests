559944576d43346d92b240f95fde2e4d2083f19e
joepetrowski <joe@parity.io>
2020-06-11 20:21:07 +0200

    add block author

--- reconcile.py
+++ reconcile.py
@@ -2,8 +2,8 @@
 import requests
 import json
 
-block = 2592628
-address = 'EFteY6Fg4BsFfWdfkS6XmsgyWLK2G3UZQnk87DsNHNcWSEW'
+block = 2621327
+address = 'GTUi6r2LEsf71zEQDnBvBvKskQcWvK66KRqcRbdmcczaadr'
 sidecar_url = 'http://127.0.0.1:8080/'
 
 def get_block(sidecar: str, block: int):
@@ -22,26 +22,41 @@ def get_balance(sidecar: str, address: str, block: int):
 		balance = json.loads(response.text)
 	return balance
 
+def is_signed_by(signature, address):
+	return signature and signature['signer'] == address
+
 def get_fees_paid_in_block(block: dict, address: str):
 	total_fees = 0
 	for xt in block['extrinsics']:
-		if xt['signature'] and xt['signature']['signer'] == address:
+		if is_signed_by(xt['signature'], address):
 			if xt['paysFee']:
 				fee = int(xt['info']['partialFee'])
 				total_fees += fee
 	return total_fees
 
-def value_transferred_in_block(block: dict, address: str):
+def value_deducted_in_block(block: dict, address: str):
+	transfer_methods = [
+		'balances.transfer',
+		'balances.transferKeepAlive',
+		'vesting.vestedTransfer'
+	]
 	value_transferred = 0
 	for xt in block['extrinsics']:
-		if xt['signature'] and xt['signature']['signer'] == address:
-			if xt['method'] == 'balances.transferKeepAlive' or xt['method'] == 'balances.transfer':
+		if is_signed_by(xt['signature'], address):
+			if xt['method'] in transfer_methods:
 				value = int(xt['args'][1])
 				value_transferred += value
 		value_transferred += value_reaped(xt['events'])
 	return value_transferred
 
 # Add function for value received.
+def value_credited_in_block(block: dict, address: str):
+	credit = 0
+	for xt in block['extrinsics']:
+		for event in xt['events']:
+			if event['method'] == 'balances.Deposit' and event['data'][0] == address:
+				credit += int(event['data'][1])
+	return credit
 
 def value_reaped(events):
 	reaped = 0
@@ -55,16 +70,22 @@ balances_before_tx = get_balance(sidecar_url, address, block-1)
 balances_after_tx = get_balance(sidecar_url, address, block)
 block_data = get_block(sidecar_url, block)
 
-pre_tx_balance = int(balances_before_tx['free'])
-post_tx_balance = int(balances_after_tx['free'])
-transfer_value = value_transferred_in_block(block_data, address)
+pre_tx_free_balance = int(balances_before_tx['free'])
+pre_tx_reserved_balance = int(balances_before_tx['reserved'])
+pre_tx_total_balance = pre_tx_free_balance + pre_tx_reserved_balance
+post_tx_free_balance = int(balances_after_tx['free'])
+post_tx_reserved_balance = int(balances_after_tx['reserved'])
+post_tx_total_balance = post_tx_free_balance + post_tx_reserved_balance
+
+transfer_value = value_deducted_in_block(block_data, address)
+credit = value_credited_in_block(block_data, address)
 fee = get_fees_paid_in_block(block_data, address)
 
-expected = pre_tx_balance - transfer_value - fee
+expected = pre_tx_total_balance - transfer_value - fee + credit
 
 print('Block {}'.format(block))
 print('Address: {}'.format(address))
-print('Actual Balance:   {:>14}'.format(post_tx_balance))
+print('Actual Balance:   {:>14}'.format(post_tx_total_balance))
 print('Expected Balance: {:>14}'.format(expected))
 print('              ------------------')
-print('Difference:       {:>14}'.format(post_tx_balance - expected))
+print('Difference:       {:>14}'.format(post_tx_total_balance - expected))
